0ea63824c087158431f6b13178d83b54ab269154
mf ismail <smlbarc@gmail.com>
2021-09-11 18:19:02 +0530

    updating the files

--- mymodel.py
+++ mymodel.py
@@ -1,50 +1,70 @@
 import pandas as pd
 import sklearn
 import numpy as np
-
-messages=pd.read_csv('C:/Users/DELL/Desktop/Machine_Learning/NLP/SpamClassifier-master/smsspamcollection/smsspamcollection',
+messages=pd.read_csv('smsspamcollection/smsspamcollection',
                      sep='\t',names=["label","message"])
 
+import gensim
 import re
 import nltk
 nltk.download('stopwords')
+nltk.download('wordnet')
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-ps=PorterStemmer()
+from nltk.stem import WordNetLemmatizer
 
-corpus=[]
+ps=PorterStemmer()
+lem=WordNetLemmatizer()
+
+corpus1=[]
 for i in range(0,len(messages)):
     review=re.sub('[^a-zA-z]'," ",messages["message"][i])
     review=review.lower()
     review=review.split()
     
-    review = [ps.stem(word) for  word in review if not word in stopwords.words('english')]
+    review = [word for  word in review if not word in stopwords.words('english')]
     review=" ".join(review)
-    corpus.append(review)
+    corpus1.append(review)
     
     
 from sklearn.feature_extraction.text import CountVectorizer
-cv=CountVectorizer(max_features=5000)
-x=cv.fit_transform(corpus).toarray()
+cv=CountVectorizer(max_features=3000)
 
+pickle.dump(cv,open('bag_of_words_transform.pkl','wb'))
+x_bag=cv.fit_transform(corpus).toarray()
 
-y=pd.get_dummies(messages['label'])  
-y=y.iloc[:,1].values
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf=TfidfVectorizer()
+pickle.dump(tfidf,open('tfidf_transform.pkl','wb'))
+
+x_tf=tfidf.fit_transform(corpus1).toarray()
+
+y_bag=pd.get_dummies(messages['label'])  
+y_bag=y_bag.iloc[:,1].values
+
+y_tfidf=pd.get_dummies(messages['label'])
+y_tfidf=y_tfidf.iloc[:,1].values
+
 
 np.random.seed(42)
 from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+x_train,x_test,y_train,y_test=train_test_split(x_tf,y_tfidf,test_size=0.2)
+
 
 from sklearn.naive_bayes import MultinomialNB
 model=MultinomialNB()  
 model.fit(x_train,y_train)
 
-y_pred=model.predict(x_test)
+y_pred_tfidf=model.predict(x_test)
 
 from sklearn.metrics import confusion_matrix
-confusion=confusion_matrix(y_test,y_pred)
+confusion1=confusion_matrix(y_test,y_pred_tfidf)
 
 
 from sklearn.metrics import accuracy_score
-accuracy=accuracy_score(y_test,y_pred)
+accuracy1=accuracy_score(y_test,y_pred_tfidf)
+
+import pickle
+filename='spam-model_tfidf.pkl'
+pickle.dump(model,open(filename,'wb'))
